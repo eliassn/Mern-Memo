bd5ce9652f09544f4e323e15630f880df6765963
eliassn <ilyes.upes@gmail.com>
2021-07-13 15:19:52 +0100

    final commit

---
+++ client/build/precache-manifest.dbb940f38e29970db8fb283cbc53ac6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2953b0bfe1c742fa3b472a6d09098be3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bce08a0bdd13c86f75bb",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "5f69ea4e1595d8d74660",
+    "url": "/static/js/2.4d6c96b3.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.4d6c96b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce08a0bdd13c86f75bb",
+    "url": "/static/js/main.c0cf92f7.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
---
+++ server/index.js
@@ -0,0 +1,26 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv'
+
+import postRoutes from './routes/posts.js';
+dotenv.config()
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect("mongodb+srv://developer:developer123@cluster0.gnsqj.mongodb.net/memo?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
